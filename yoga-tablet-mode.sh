d747f46db0d926ba0d5d4bda06298571f97dba24
itzevvv <76193592+itzevvv@users.noreply.github.com>
2025-12-14 23:08:42 -0600

    Create yoga-tablet-mode.sh

---
+++ yoga-tablet-mode.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+SCREEN="eDP-1"
+STATE_FILE="/tmp/yoga_tablet_mode"
+ORIENTATION_FILE="/tmp/yoga_orientation"
+STARTUP_FILE="/tmp/yoga_startup_reload"
+
+LAST_ORIENTATION="normal"
+
+function background_read_state {
+    while true; do
+        evtest "/dev/input/tablet-mode-switch" | grep -Po --line-buffered 'SW_TABLET_MODE.*\K\d+' | while read state; do
+            if [[ "$state" =~ ^[0-1]$ ]]; then
+                prev_state=$(cat "$STATE_FILE")
+                echo "states now $state!"
+                echo "$state" > $STATE_FILE
+                
+                if [ "${state}" != "${prev_state}" ]; then
+                    echo "state changed from $prev_state to $state"
+
+                    if [[ $state -eq 0 ]]; then
+                        echo "LAPTOP MODE!"
+                        rotate "normal"
+                    else
+                        echo "state change; reorienting"
+                        echo "$LAST_ORIENTATION"
+                        rotate $(cat $ORIENTATION_FILE)
+                    fi
+                fi
+	        else
+		        echo "hm,,that wrong"
+            fi
+        done
+    done
+}
+
+function rotate {
+    case $1 in
+        "normal")
+            ROTATION="normal"
+            ;;
+        "right-up")
+            ROTATION="right"
+            ;;
+        "bottom-up")
+            ROTATION="inverted"
+            ;;
+        "left-up")
+            ROTATION="left"
+            ;;
+    esac
+
+    echo "received rotation: $ROTATION"
+
+    kscreen-doctor "output.$SCREEN.rotation.$ROTATION"
+}
+
+function check_startup {
+    echo "checking startup"
+    if [[ ! -f "$STARTUP_FILE" ]]; then
+        echo "startup file not found, restarting amd_pmf and amd_sfh..."
+        startup
+        touch "$STARTUP_FILE"
+    else
+        echo "already started up!"
+    fi
+}
+
+function startup {
+    echo "wait..."
+    sudo modprobe -r amd_pmf amd_sfh
+    sleep 1
+    sudo modprobe amd_sfh amd_pmf
+    sleep 4
+    echo "wait ok"
+}
+
+check_startup
+background_read_state &
+
+trap 'trap - SIGTERM && kill 0' SIGINT SIGTERM EXIT
+
+while IFS='$\n' read -r line; do
+    rotation="$(echo $line | sed -En "s/^.*orientation changed: (.*)/\1/p")"
+    if [[ ! -z $rotation ]]; then
+        LAST_ORIENTATION="$rotation"
+
+        echo "$rotation" > $ORIENTATION_FILE
+
+        echo "orientation is now stored as $LAST_ORIENTATION"
+        
+        mode=$(cat "$STATE_FILE")
+        
+        if [[ $mode == "1" ]]; then
+            echo "yoga in tablet mode, rotating"
+            rotate $rotation
+        fi
+    fi
+done < <(stdbuf -oL monitor-sensor)
+
